94a5ce6893a5bf0422d0728e8b5d66b338f48ca6
Manivardhanreddy-1504 <manivardhanreddy986@gmail.com>
2025-09-23 10:33:15 +0000

    main file change

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'Welcome to python Flask World v3.0'
+	return 'Welcome to python Flask World v1.0 from CI/CD Pipeline'
 
 
 if __name__ == '__main__':
